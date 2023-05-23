2aad6bed4bcd8ade8562dfb3fe719d89ff245da3
Game K <57108380+Game-K-Hack@users.noreply.github.com>
2023-05-23 16:27:07 +0200

    API edited

--- api.py
+++ api.py
@@ -1,10 +1,11 @@
 import os
-import requests
 from fake_useragent import UserAgent
 from utils import download as dld
 
 import cfscrape
 import cloudscraper
+
+import requests
 
 
 CFSCRAPE = cfscrape.create_scraper()
@@ -59,3 +60,69 @@ class InstaStories:
                                               "False").replace("true", "True"))
         self.stories = {i["id"]: i["src"] for i in res["result"]["playlist"]}
         return len(self.stories)
+    
+class response:
+    def __init__(self, username) -> None:
+        self.username = username
+        self.story = list()
+        self.path = f"./download/{username}/"
+        try: os.makedirs(self.path)
+        except: pass
+
+    def setStory(self, urls:list) -> None:
+        self.story = urls
+
+    def addStory(self, name:str, url:str) -> None:
+        self.story.append({
+            "name":name, 
+            "url":url
+        })
+    
+    def download(self) -> None:
+        for i, url in enumerate(self.story):
+            print(f"[*] Download... {(100*i) // len(self.story)} %", end="\r")
+            with requests.get(url["url"], stream=True) as res:
+                res.raise_for_status()
+                with open(self.path + url["name"], "wb") as file:
+                    for chunk in res.iter_content(chunk_size=8192): 
+                        file.write(chunk)
+        print(f"[+] Downloaded 100 %")
+    
+class instasupersave:
+    def __init__(self, username, id, scraper) -> None:
+        self.username = username
+        self.id = str(id)
+        self.resObject = response(username)
+        self.scraper = scraper
+
+    def getStory(self) -> response:
+        # 
+        res = self.scraper.get(f"https://instasupersave.com/api/ig/stories/{self.id}")
+        # Decode response of the request
+        res = res.content.decode("utf8")
+        # Convert str to dict (json)
+        res = eval(res.replace("true", "True").replace("false", "False"))
+        # Add in response object
+        for story in res["result"]:
+            # 
+            if "video_versions" in story.keys():
+                k, ext = "video_versions", ".mp4"
+            elif "image_versions2" in story.keys():
+                k, ext = "image_versions2", ".png"
+            else:
+                raise "Key not found"
+            # 
+            if type(story[k]) != list and "candidates" in story[k].keys():
+                story[k] = story[k]["candidates"]
+            # Search the high quality of video or photo
+            index, h = 0, 0
+            for i, elm in enumerate(story[k]):
+                if elm["height"] > h:
+                    h = elm["height"]
+                    index = i
+            # The index the place of the high quality in list
+            self.resObject.addStory(
+                name = story["pk"] + ext, 
+                url = story[k][index]["url"])
+        # 
+        return self.resObject
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from fake_useragent import UserAgent
 # class Core:
 #     login = "Votre utilisateur"
 #     paswd = "Votre mot de passe"
-#     user_list = ["user1", "user2", ...]
+#     user_list = [{"username": "user1", "id": id1}, {"username": "user2", "id": id2}, ...]
 # or import lib
 from core import Core
 
@@ -20,6 +20,8 @@ m = mega.login(Core.login, Core.paswd)
 
 L = instaloader.Instaloader(user_agent=UserAgent().firefox)
 
+file_limit = 50
+
 space = m.get_storage_space(kilo=True)
 if space["used"] > space["total"]-10000:
     print("[!] Storage soon full")
@@ -28,13 +30,13 @@ current_time = str(datetime.datetime.now()).split(" ")[0]
 
 for username in Core.user_list:
 
-    print(f"[*] Scan {username} profile...")
+    print(f"[*] Scan {username['username']} profile...")
 
-    folder = m.find(username, exclude_deleted=True)
+    folder = m.find(username['username'], exclude_deleted=True)
     if folder is None:
-        m.create_folder(username)
+        m.create_folder(username['username'])
 
-    profile = instaloader.Profile.from_username(L.context, username)
+    profile = instaloader.Profile.from_username(L.context, username['username'])
 
     print("[*] Get post...")
     posts_dict = {}
@@ -65,35 +67,50 @@ for username in Core.user_list:
     for key in posts_dict.keys():
         pbar = utils.progress_bar(len(posts_dict[key].keys())+1)
         for post in posts_dict[key]:
-            utils.download(post["url"], f"./download/{username}/{post['name']}.{'mp4' if post['is_video'] else 'jpg'}")
+            utils.download(post["url"], f"./download/{username['username']}/{post['name']}.{'mp4' if post['is_video'] else 'jpg'}")
             print(f"[*] ({key}) Download {pbar.update()}", end='\r')
         print(f"\r[+] ({key}) Downloaded {pbar.update()}")
 
+
     print("[*] Get stories...")
-    # Set profile
-    insta = api.InstaStories(username)
-    insta.set_scraper(api.CFSCRAPE)
-    # Scan storie on save-insta-com website
-    insta.save_insta_com()
-    # Download storie
-    insta.download()
-    print("[+] Stories scanned")
+    # # Set profile
+    # insta = api.InstaStories(username)
+    # insta.set_scraper(api.CFSCRAPE)
+    # # Scan storie on save-insta-com website
+    # insta.save_insta_com()
+    # # Download storie
+    # insta.download()
 
-    if len(os.listdir(f"./download/{username}")) != 0:
-        print("[*] Upload on Mega...")
-        dirname = username + "/" + str(datetime.datetime.now()).split(" ")[0]
+    try:
+        insta = api.instasupersave(
+            username=username['username'],
+            id=username["id"],
+            scraper=api.CFSCRAPE)
+        res = insta.getStory()
+        res.download()
+    except:
+        pass
+
+    print("[+] Stories scanned\n[*] Upload on Mega...")
+
+    dirname = username['username'] + "/" + str(datetime.datetime.now()).split(" ")[0]
+    folder = m.find(dirname, exclude_deleted=True)
+    if folder is None:
+        m.create_folder(dirname)
         folder = m.find(dirname, exclude_deleted=True)
-        if folder is None:
-            m.create_folder(dirname)
-            folder = m.find(dirname, exclude_deleted=True)
-        pbar = utils.progress_bar(len(os.listdir(f"./download/{username}"))+1)
-        for file in os.listdir(f"./download/{username}"):
-            if not m.find(file, exclude_deleted=True):
-                m.upload(f"./download/{username}/{file}", folder[0])
-            else:
-                print(f"[!] Already exists (file:{file})", " "*50)
-            print(f"[*] Upload {pbar.update()}", end='\r')
-            os.remove(f"./download/{username}/{file}")
-        print(f"\r[+] Uploaded {pbar.update()} ")
+    pbar = utils.progress_bar(len(os.listdir(f"./download/{username['username']}"))+1)
+    for file in os.listdir(f"./download/{username['username']}"):
+        if not m.find(file, exclude_deleted=True):
+            m.upload(f"./download/{username['username']}/{file}", folder[0])
+        else:
+            print(f"[!] Already exists (file:{file})", " "*50)
+        print(f"[*] Upload {pbar.update()}", end='\r')
+    print(f"\r[+] Uploaded {pbar.update()} ")
 
-    print(f"[+] Profile {username} scanned\n")
+    print(f"[*] Clear old photo", end='\r')
+    
+    files = os.listdir(f"./download/{username['username']}")
+    for i in [x for x in files if x not in files[-file_limit:]]:
+        os.remove(f"./download/{username['username']}/{i}")
+
+    print(f"[+] Clear old photo\n[+] Profile {username['username']} scanned\n")
